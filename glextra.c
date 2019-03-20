231017339592cac5b8611ec3e0f9a55d515af775
Lydia Chan <lchan528@stanford.edu>
2019-03-20 10:38:09 -0700

    Implemented midpoint algorithm for circle drawing.

--- glextra.c
+++ glextra.c
@@ -1,49 +1,70 @@
 #include "glextra.h"
 #include "gl.h"
 #include "timer.h"
+#include "printf.h"
 #include "math.h"
 
-/* Helper function to drawCircle(). This function draws in all 8 octaves of a 
-circle derived from Bresenham's Circle Algorithm.
- */
-static void drawOuterPoints(int xc, int yc, int x, int y, color_t color) {
-  gl_draw_pixel(xc+x, yc+y, color);
-  gl_draw_pixel(xc-x, yc+y, color);
-  gl_draw_pixel(xc+x, yc-y, color);
-  gl_draw_pixel(xc-x, yc-y, color);
-  gl_draw_pixel(xc+y, yc+x, color);
-  gl_draw_pixel(xc-y, yc+x, color);
-  gl_draw_pixel(xc+y, yc-x, color);
-  gl_draw_pixel(xc-y, yc-x, color);
-}
+struct Circle* head;
 
-/*Draw circles using Bresenham's algorithm*/
+
+/*Draw circles with Mid-Point Algorithm*/
 void drawCircle(int xCenter, int yCenter, int radius, color_t c) {
-  int x = 0;
-  int y = radius;
-  int decision = 3 - (2*radius);
-  drawOuterPoints(xCenter, yCenter, x, y, c);
+  int x = radius;
+  int y = 0;
 
-  while (y > x) {
-    x++;
-    if (decision > 0) {
-      y--;
-      decision += (4*(x-y) + 10);
+  //draw 4 outerpoints
+  gl_draw_pixel(xCenter+x, yCenter+y, c);
+  gl_draw_pixel(xCenter+y, yCenter+x, c);
+  gl_draw_pixel(xCenter-x, yCenter-y, c);
+  gl_draw_pixel(xCenter-y, yCenter-x, c);
+  
+  int P = (1-radius);
+  while (x > y) {
+    y++;
+
+    if (P <= 0) {
+      P += (2*y + 1);
     } else {
-      decision += ((4*x) + 6);
+      x--;
+      P += (2*y - 2*x + 1);
     }
-    drawOuterPoints(xCenter, yCenter, x, y, c);
-  } 
-}
 
+    if (x < y) {
+      break;
+    }
+
+    //draw point in all four quadrants
+    gl_draw_pixel(xCenter+x, yCenter+y, c);
+    gl_draw_pixel(xCenter-x, yCenter+y, c);
+    gl_draw_pixel(xCenter+x, yCenter-y, c);
+    gl_draw_pixel(xCenter-x, yCenter-y, c);
+
+    if (x != y) {
+      //draw reflected point over y=x in all four quadrants
+      gl_draw_pixel(xCenter+y, yCenter+x, c);
+      gl_draw_pixel(xCenter-y, yCenter+x, c);
+      gl_draw_pixel(xCenter+y, yCenter-x, c);
+      gl_draw_pixel(xCenter-y, yCenter-x, c);
+    }
+
+  }
+
+}
 
 /*
 
  */
 struct Circle* calculateHead(const char* data){
-  struct Circle* head;
+  // struct Circle* head = (struct Circle*)malloc(sizeof(struct Circle)); //malloc
+  head->radius = 0;
+  struct Point headCenter = {15, 15};
+  head->center = headCenter;
   return head;
   /* To be implemented in glextra.c */
+}
+
+void printHead() {
+  printf("HEAD cntr (%d,%d), rad=%d\n", head->center.x, head->center.y, head->radius);
 }
 
 void projectInfraredDataToMonitor(short* infraredData, int rowSize){
@@ -56,16 +77,53 @@ void projectInfraredDataToMonitor(short* infraredData, int rowSize){
     }
     //Quik maths
     unsigned char red = 0;
+    unsigned char green = 0;//test
     unsigned char blue = 0;
-    if(*infraredData/4 > 22){
+    if(*infraredData/4 > 24) {
+      green = *infraredData;
+    } else if(*infraredData/4 > 22){
       red = *infraredData;
-    }
-    else{
+    } else{
       blue = *infraredData;
     }
-    gl_draw_pixel(col%rowSize,row,gl_color(red,0,blue));
+    gl_draw_pixel(col%rowSize,row,gl_color(red,green,blue));
     infraredData++; 
   }
   gl_swap_buffer();
 }
+
+
+
+// /* Helper function to drawCircle(). This function draws in all 8 octaves of a 
+// circle derived from Bresenham's Circle Algorithm.
+//  */
+// static void drawOuterPoints(int xc, int yc, int x, int y, color_t color) {
+//   gl_draw_pixel(xc+x, yc+y, color);
+//   gl_draw_pixel(xc-x, yc+y, color);
+//   gl_draw_pixel(xc+x, yc-y, color);
+//   gl_draw_pixel(xc-x, yc-y, color);
+//   gl_draw_pixel(xc+y, yc+x, color);
+//   gl_draw_pixel(xc-y, yc+x, color);
+//   gl_draw_pixel(xc+y, yc-x, color);
+//   gl_draw_pixel(xc-y, yc-x, color);
+// }
+
+// /*Draw circles using Bresenham's algorithm*/
+// void drawCircle(int xCenter, int yCenter, int radius, color_t c) {
+//   int x = 0;
+//   int y = radius;
+//   int decision = 3 - (2*radius);
+//   drawOuterPoints(xCenter, yCenter, x, y, c);
+
+//   while (y > x) {
+//     x++;
+//     if (decision > 0) {
+//       y--;
+//       decision += (4*(x-y) + 10);
+//     } else {
+//       decision += ((4*x) + 6);
+//     }
+//     drawOuterPoints(xCenter, yCenter, x, y, c);
+//   } 
+// }
 
